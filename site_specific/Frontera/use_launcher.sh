e1094d9ac7698c87648acddadcb3dc2be35e0824
Dirk Hufnagel <Dirk.Hufnagel@cern.ch>
2021-07-15 23:44:01 +0200

    add Frontera launcher and cvmfs wrapper

---
+++ site_specific/Frontera/use_launcher.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+module load launcher
+
+NUMBEROFNODES=28
+
+export LAUNCHER_WORKDIR=/home1/05501/uscms/launcher
+
+echo "DEBUG: LAUNCHER_WORKDIR=$LAUNCHER_WORKDIR"
+
+export LAUNCHER_JOB_FILE=`mktemp -p $LAUNCHER_WORKDIR 'jobfile.XXXXXXXX'`
+
+echo "DEBUG: LAUNCHER_JOB_FILE=$LAUNCHER_JOB_FILE"
+
+for i in $(seq $NUMBEROFNODES)
+do
+    echo "$LAUNCHER_WORKDIR/node_wrapper.sh $@" >> $LAUNCHER_JOB_FILE
+done
+
+$LAUNCHER_DIR/paramrun
+
+rm -f $LAUNCHER_JOB_FILE
